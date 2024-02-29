0c959f8f169f8947f5e8d99ce8dd79a9127d7bd1
eirik <eirikoa@uio.no>
2024-02-29 13:34:30 +0100

    Oppretting av fil

---
+++ app/src/main/java/no/uio/ifi/in2000/eirikoa/team50test/MainActivity.kt
@@ -0,0 +1,46 @@
+package no.uio.ifi.in2000.eirikoa.team50test
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import no.uio.ifi.in2000.eirikoa.team50test.ui.theme.Team50TestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Team50TestTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Team50TestTheme {
+        Greeting("Android")
+    }
+}
