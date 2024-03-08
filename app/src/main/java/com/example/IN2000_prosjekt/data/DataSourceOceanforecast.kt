775719c3051614e77cf75d40d79716da08f16012
Ayuub Abdulkadir Hassan <ayuubh353@gmail.com>
2024-03-08 16:10:44 +0100

    adding a  temporary method that retrieves the api

--- app/src/main/java/com/example/IN2000_prosjekt/data/DataSourceOceanforecast.kt
+++ app/src/main/java/com/example/IN2000_prosjekt/data/DataSourceOceanforecast.kt
@@ -7,23 +7,27 @@ import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.request.get
 import io.ktor.serialization.gson.gson
 import io.ktor.util.appendIfNameAbsent
+import kotlinx.coroutines.runBlocking
 
 class DataSourceOceanforecast() {
     private val client = HttpClient() {
         defaultRequest {
-            url("https://gw-uio.intark.uh-it.no/in2000/weatherapi")
-            headers.appendIfNameAbsent("X-Gravitee-API-Key", "86d1e8ef-7703-4ded-b17a-f168226135cb")
+            url("https://gw-uio.intark.uh-it.no/in2000/")
+            headers.appendIfNameAbsent(
+                "X-Gravitee-API-Key",
+                "86d1e8ef-7703-4ded-b17a-f168226135cb"
+            ) //appendifnameabsent passer på at det ikke legger til to header
         }
         install(ContentNegotiation) {
             gson()
+
         }
     }
 
     suspend fun fetchOceanForecast(): oceanForecastData {
-        val oceanResponse = client.get("/oceanforecast/2.0/complete?lat=60.10&lon=5")
-        println(oceanResponse.body<oceanForecastData>())
+        val oceanResponse = client.get("weatherapi/oceanforecast/2.0/complete?lat=60.10&lon=5")
         return oceanResponse.body<oceanForecastData>()
 
-
     }
+
 }
