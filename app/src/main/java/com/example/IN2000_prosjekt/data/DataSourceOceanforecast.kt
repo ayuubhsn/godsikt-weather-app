fc859bfdc76041ac976718e37a49db94702192b7
samat <samatarh@ifi.uio.no>
2024-03-10 18:45:09 +0000

    soveld merge problem

--- app/src/main/java/com/example/IN2000_prosjekt/data/DataSourceOceanforecast.kt
+++ app/src/main/java/com/example/IN2000_prosjekt/data/DataSourceOceanforecast.kt
@@ -11,30 +11,22 @@ import io.ktor.util.appendIfNameAbsent
 class DataSourceOceanforecast() {
     private val client = HttpClient() {
         defaultRequest {
-            url("https://gw-uio.intark.uh-it.no/in2000/weatherapi")
-            headers.appendIfNameAbsent("X-Gravitee-API-Key", "86d1e8ef-7703-4ded-b17a-f168226135cb")
+            url("https://gw-uio.intark.uh-it.no/in2000/")
+            headers.appendIfNameAbsent(
+                "X-Gravitee-API-Key",
+                "86d1e8ef-7703-4ded-b17a-f168226135cb"
+            )
         }
         install(ContentNegotiation) {
             gson()
-        }
 
+        }
     }
 
     suspend fun fetchOceanForecast(): oceanForecastData {
-        val oceanResponse = client.get("/oceanforecast/2.0/complete?lat=60.10&lon=5")
-        println(oceanResponse.body<oceanForecastData>())
+        val oceanResponse = client.get("weatherapi/oceanforecast/2.0/complete?lat=60.10&lon=5")
         return oceanResponse.body<oceanForecastData>()
 
-
-<<<<<<< Updated upstream
     }
-}
-=======
-}
-suspend fun main(){
-    val oceanforecast:DataSourceOceanforecast= DataSourceOceanforecast()
-    println(oceanforecast.fetchOceanForecast())
-}
 
-
->>>>>>> Stashed changes
+}
