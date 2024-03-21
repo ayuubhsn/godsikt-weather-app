409538278e13789fb1c94f7a7a50ae63d010326b
Ayuub Abdulkadir Hassan <ayuubh353@gmail.com>
2024-03-21 08:09:45 +0100

    adding navigation dependencies in build gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -78,4 +78,21 @@ dependencies {
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-gson:$ktor_version")
     implementation("io.coil-kt:coil-compose:2.6.0")
+
+    //navigasjon
+
+    val nav_version = "2.7.7"
+    // Java language implementation
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+    // Kotlin
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    // Feature module Support
+    implementation("androidx.navigation:navigation-dynamic-features-fragment:$nav_version")
+    // Testing Navigation
+    androidTestImplementation("androidx.navigation:navigation-testing:$nav_version")
+    // Jetpack Compose Integration
+    implementation("androidx.navigation:navigation-compose:$nav_version")
+
 }
