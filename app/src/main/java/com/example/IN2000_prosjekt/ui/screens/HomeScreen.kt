373f4431c50301acda93ae0196ade24935a19b6b
Ayuub Abdulkadir Hassan <ayuubh353@gmail.com>
2024-03-21 08:13:47 +0100

    addin some new function in homescreen, (buttons and navigation system between the screens)

--- app/src/main/java/com/example/IN2000_prosjekt/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/IN2000_prosjekt/ui/screens/HomeScreen.kt
@@ -1,14 +1,72 @@
 package com.example.IN2000_prosjekt.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
 
-class HomeScreen{
 
 @Composable
-fun StartScreen(){
+fun HomeScreen(navController: NavController){
+    Box(
+        modifier = Modifier
+            //.fillMaxSize()
+            .padding(16.dp),
+        contentAlignment = Alignment.BottomCenter
+    ){
+        Text(
+            text = "Velkommen til Trygg Sjø",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium,
+            modifier = Modifier.align(Alignment.TopCenter)
+        )
+        Row (
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+            //knapp 1
+            Button(
+                onClick = { navController.navigate("MapScreen") },
+                colors = ButtonDefaults.buttonColors(Color(0xFF000080))
+            ) {
+                Text(text = "Kart")
+            }
+
+            //knapp 2
+            Button(
+                onClick = { navController.navigate("") },
+                colors = ButtonDefaults.buttonColors(Color(0xFF000080))
+            ) {
+                Text(text = "API")
+            }
 
 
 
+
+        }
     }
 }
