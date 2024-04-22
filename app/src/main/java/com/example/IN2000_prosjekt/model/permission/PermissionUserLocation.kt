7e118de903f6525b87500fc47b41e8245ad2c7b6
Ayuub Abdulkadir Hassan <ayuubh353@gmail.com>
2024-04-22 11:26:18 +0200

    adding a new package that includees the permission for gps

---
+++ app/src/main/java/com/example/IN2000_prosjekt/model/permission/PermissionUserLocation.kt
@@ -0,0 +1,50 @@
+package com.example.IN2000_prosjekt.utils
+
+
+
+import android.app.Activity
+import android.util.Log
+
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
+import com.example.IN2000_prosjekt.model.weather.MapViewModel
+
+
+
+class PermissionUserLocation(private val activity: Activity, private val mapViewModel: MapViewModel) {
+
+    private val permissionsManager = PermissionsManager(object : PermissionsListener {
+        override fun onExplanationNeeded(permissionsToExplain: List<String>) {
+        }
+
+        override fun onPermissionResult(granted: Boolean) {
+            if (granted) {
+                onPermissionsGranted()
+            } else {
+                onPermissionsDenied()
+            }
+        }
+    })
+
+    fun checkAndRequestLocationPermissions() {
+        if (PermissionsManager.areLocationPermissionsGranted(activity)) {
+            onPermissionsGranted()
+        } else {
+            permissionsManager.requestLocationPermissions(activity)
+        }
+    }
+
+    fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        permissionsManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun onPermissionsGranted() {
+        Log.d("PermissionUserLocation", "Plasseringstillatelsene er bekreftet")
+        mapViewModel.updateLocationPermissions(true)
+    }
+
+    private fun onPermissionsDenied() {
+        mapViewModel.updateLocationPermissions(false)
+    }
+}
+
