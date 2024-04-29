cb460136d70a88654059c35b4aab78c2fbc712a5
Ayuub Abdulkadir Hassan <ayuubh353@gmail.com>
2024-04-29 13:04:38 +0200

    adding som new adjumenst for locationforecast and metalert

--- app/src/main/java/com/example/IN2000_prosjekt/data/WeatherRepository.kt
+++ app/src/main/java/com/example/IN2000_prosjekt/data/WeatherRepository.kt
@@ -1,11 +1,16 @@
 package com.example.IN2000_prosjekt.data
 
+import DataSourceLocationforecast
+import android.util.Log
+import com.example.IN2000_prosjekt.ui.LocationInfo
+import com.example.IN2000_prosjekt.ui.MetAlert
+
 
 class WeatherRepository {
     val dataSourceMetAlert : DataSourceMetAlert = DataSourceMetAlert()
     val dataSourceOceanforecast : DataSourceOceanforecast = DataSourceOceanforecast()
     val dataSourceHavvarsel: DataSourceHavvarsel = DataSourceHavvarsel()
-
+    val dataSourceLocationforecast : DataSourceLocationforecast =DataSourceLocationforecast()
     suspend fun getTemprature(lat:String= "60.10", lon:String = "5") : String{
         val tempratur = dataSourceOceanforecast.fetchOceanForecast(lat,lon).properties.timeseries.getOrNull(0)?.data?.instant?.details?.sea_water_temperature
         return  tempratur.toString()
@@ -50,7 +55,66 @@ class WeatherRepository {
     }
 
 
+    suspend fun getMetAlert(
+        lat: String,
+        lon: String
+    ): MetAlert {
+        val metAlert = dataSourceMetAlert.fetchMetAlert( lon,lat)
+
+        val info =
+            metAlert.features.firstOrNull()?.properties?.title ?: "No title available"
+        val color =
+            metAlert.features.firstOrNull()?.properties?.riskMatrixColor ?: "No color available"
+        val level =
+            metAlert.features.firstOrNull()?.properties?.awareness_level
+        val description =
+            metAlert.features.firstOrNull()?.properties?.description
+
+        return MetAlert(
+            riskMatrixColor = color,
+            description = description!!
+        )
+    }
 
 
+    suspend fun getLocation(
+        lat: String,
+        lon: String
+    ): LocationInfo {
+        val locationForecast = dataSourceLocationforecast.fetchLocationforecast(lat, lon)
+
+        val temp =
+            locationForecast.properties.timeseries.getOrNull(0)?.data?.instant?.details?.air_temperature?.toInt()
+        val wind =
+            locationForecast.properties.timeseries.firstOrNull()?.data?.instant?.details?.wind_speed
+        val rain =
+            locationForecast.properties.timeseries.getOrNull(0)?.data?.next_1_hours?.details?.precipitation_amount
+
+        val rainSymbol =
+            locationForecast.properties.timeseries.getOrNull(0)?.data?.next_1_hours?.summary?.symbol_code
+        val fog =
+            locationForecast.properties.timeseries.getOrNull(0)?.data?.instant?.details?.fog_area_fraction
+        val windLocation =
+            locationForecast.properties.timeseries.getOrNull(0)?.data?.instant?.details?.wind_from_direction
+
+        Log.d("rep", "$rain")
+        Log.d("rep", "$wind")
+        Log.d("rep", "$temp")
+        Log.d("rep", "$windLocation")
+
+
+        return LocationInfo(
+            temperatureL = temp!!,
+            fog_area_fraction = fog!!,
+            precipitation_amount = rain!!,
+            wind_speed = wind!!,
+            wind_from_direction = windLocation!!,
+            symbol_code = rainSymbol!!
+
+        )
+    }
 
 }
+
+
+
