222a7ed552d6c499c7910173a098f0795d5955cf
samat <samatarh@ifi.uio.no>
2024-05-08 19:33:23 +0100

    comment out logs

--- app/src/main/java/com/example/IN2000_prosjekt/data/weather/WeatherRepository.kt
+++ app/src/main/java/com/example/IN2000_prosjekt/data/weather/WeatherRepository.kt
@@ -13,31 +13,31 @@ class WeatherRepository {
         lon: String
     ): MetAlert {
 
-        Log.d("WeatherRepo", "getMetAlert $lat, $lon")
+        //Log.d("WeatherRepo", "getMetAlert $lat, $lon")
 
         val metAlert = dataSourceMetAlert.fetchMetAlert( lon,lat)
 
-        Log.d("WeatherRepo", "datasourcemetalert")
+        //Log.d("WeatherRepo", "datasourcemetalert")
 
         val info =
             metAlert.features.firstOrNull()?.properties?.title ?: "No title available"
 
-        Log.d("WeatherRepo", "info")
+       // Log.d("WeatherRepo", "info")
 
         val color =
             metAlert.features.firstOrNull()?.properties?.riskMatrixColor ?: "No color available"
 
-        Log.d("WeatherRepo", "color")
+        //Log.d("WeatherRepo", "color")
 
         val level =
             metAlert.features.firstOrNull()?.properties?.awareness_level
 
-        Log.d("WeatherRepo", "level")
+       // Log.d("WeatherRepo", "level")
 
         val description =
             metAlert.features.firstOrNull()?.properties?.description?: "Ingen data tilgjengelig for valgt posisjon"
 
-        Log.d("WeatherRepo", "description")
+        //Log.d("WeatherRepo", "description")
 
 
         return MetAlert(
@@ -51,10 +51,10 @@ class WeatherRepository {
         lat: String,
         lon: String
     ): LocationInfo {
-        Log.d("WeatherRepo", "getLocation")
+        //Log.d("WeatherRepo", "getLocation")
         val locationForecast = dataSourceLocationforecast.fetchLocationforecast(lat, lon)
 
-        Log.d("WeatherRepo", "getlocation2")
+        //Log.d("WeatherRepo", "getlocation2")
 
         val temp =
             locationForecast.properties.timeseries.getOrNull(0)?.data?.instant?.details?.air_temperature?.toInt()
@@ -63,7 +63,7 @@ class WeatherRepository {
         val rain =
             locationForecast.properties.timeseries.getOrNull(0)?.data?.next_1_hours?.details?.precipitation_amount
 
-        Log.d("WeatherRepo", "$rain")
+       // Log.d("WeatherRepo", "$rain")
 
         val rainSymbol =
             locationForecast.properties.timeseries.getOrNull(0)?.data?.next_1_hours?.summary?.symbol_code
